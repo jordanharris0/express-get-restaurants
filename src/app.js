34e770ecc2866032a6fc24097203644d97abb0d8
jordanharris0 <jordanharris1833@yahoo.com>
2025-05-19 14:03:24 -0500

    added CRUD routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index");
+const Restaurant = require("../models/Restaurant");
 const db = require("../db/connection");
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 //TODO: Create your GET Request Route Below:
 app.get("/restaurants", async (req, res) => {
   const restaurants = await Restaurant.findAll();
   res.json(restaurants);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+  const id = req.params.id;
+  const restaurant = await Restaurant.findByPk(id);
+  res.json(restaurant);
+});
+
+app.post("/restaurants", async (req, res) => {
+  const restaurant = await Restaurant.create(req.body);
+  res.status(201).json(restaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const id = req.params.id;
+  const restaurant = await Restaurant.findByPk(id);
+  await restaurant.update(req.body);
+  res.status(201).json(restaurant);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  const id = req.params.id;
+  const restaurant = await Restaurant.findByPk(id);
+  await restaurant.destroy();
+  res.status(204).send();
 });
 
 module.exports = app;
